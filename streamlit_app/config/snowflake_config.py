ab994475099234c482b651be5b1f1aa6aaddda0d
Sameer Kadu <Samirkadu8@gmail.com>
2025-05-11 23:52:02 +0530

    environment veriable set

--- streamlit_app/config/snowflake_config.py
+++ streamlit_app/config/snowflake_config.py
@@ -1,17 +1,16 @@
 import streamlit as st
 import snowflake.connector
 
-# Snowflake connection function
+# Snowflake connection function using Streamlit secrets
 def get_snowflake_connection():
-    # In a real application, these would be stored securely and not hardcoded
     try:
         conn = snowflake.connector.connect(
-            user='samir12',
-            password='SamirSubodh@12',
-            account='kjzwhoh-wz60091',
-            warehouse='COMPUTE_WH',
-            database='HERO_DB',
-            schema='PUBLIC'
+            user=st.secrets["snowflake"]["user"],
+            password=st.secrets["snowflake"]["password"],
+            account=st.secrets["snowflake"]["account"],
+            warehouse=st.secrets["snowflake"]["warehouse"],
+            database=st.secrets["snowflake"]["database"],
+            schema=st.secrets["snowflake"]["schema"]
         )
         return conn
     except Exception as e:
